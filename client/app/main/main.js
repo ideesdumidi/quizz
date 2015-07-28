a242a937427619102316a250189566f2ce572914
Fiorelli pierre <pierre.fiorelli@axa.fr>
2015-07-28 18:48:52 +0200

    Signed-off-by: Fiorelli pierre <pierre.fiorelli@axa.fr>

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('quizzApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
